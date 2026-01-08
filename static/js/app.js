babab3c291212cf93b77b7dd2f103e18980cf075
Emannuh <emhmanuh@gmail.com>
2026-01-08 15:11:24 +0300

    added styling

---
+++ static/js/app.js
@@ -0,0 +1,7 @@
+// Place for small project-wide JS. Bootstrap bundle is already included.
+// Example: enable tooltips globally if used.
+
+document.addEventListener('DOMContentLoaded', () => {
+  const tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.forEach(el => new bootstrap.Tooltip(el));
+});
